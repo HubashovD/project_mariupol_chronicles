9f78945a839ee9e09cfd2a2118bb53e1e81bab21
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-03-03 15:02:52 +0200

    update

--- js/map.js
+++ js/map.js
@@ -4,5 +4,24 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var frontLineStyles = {
+    "color": "#f3a6b2",
+    "fillColor": "#f3a6b2",
+    "stroke-width": 10
+};
+
+fetch("./maps/occupied.geojson").then(function(response) {
+    return response.json();
+}).then(function(data) {
+    geoData = L.geoJSON(data, {
+        style: frontLineStyles,
+    })
+    geoData.setStyle({ 'className': 'occupied' })
+    geoData.addTo(map);
+});
+
+
+
+
 // var controls = L.control.layers(baseLayers, overlays, options);
 map.removeControl(map.zoomControl);
--- js/scrolly.js
+++ js/scrolly.js
@@ -7,8 +7,25 @@
          step: ".step",
      })
      .onStepEnter((response) => {
+
+         var date = response.element.attributes.date.nodeValue
+
+         var prev_date = parseInt(date.slice(-2, )) - 1
+
+         console.log(date)
+         console.log('.frontline' + date.slice(0, -2) + prev_date.toString())
+
+         d3.selectAll('.frontline' + date.slice(0, -2) + prev_date.toString())
+             .transition()
+             .duration(500)
+             .remove()
+
+
+
+
+
          map.flyTo([47.11, 37.57], 12);
-         console.log(response.index)
+         console.log("enter: " + response.index)
          console.log(response.element.attributes)
 
          d3.select("#date-placeholder").text(response.element.attributes[3].nodeValue)
@@ -45,19 +62,63 @@
              };
 
 
+             //  var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+             //      g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+             //  console.log("https://raw.githubusercontent.com/HubashovD/project_mariupol_chronicles/main/maps/" + response.element.attributes[3].nodeValue + ".geojson")
+
+             //  d3.json("/maps/" + response.element.attributes.date.nodeValue + ".geojson", function(data) {
+             //      console.log(data)
+             //  })
+
+             //  d3.json("/maps/" + response.element.attributes.date.nodeValue + ".geojson", function(collection) {
+             //      console.log(collection)
 
 
-             fetch("./maps/" + response.element.attributes[3].nodeValue + ".geojson")
-                 .then(function(response) {
-                     return response.json();
-                 })
-                 .then(function(data) {
-                     geoData = L.geoJSON(data, {
-                         style: frontLineStyles,
+             //  function projectPoint(x, y) {
+             //      var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+             //      this.stream.point(point.x, point.y);
+             //  }
+
+             //  var transform = d3.geo.transform({ point: projectPoint }),
+             //      path = d3.geo.path().projection(transform);
+
+             //  var feature = g.selectAll("path")
+             //      .data(collection.features)
+             //      .enter().append("path");
+
+             //  feature.attr("d", path);
+
+             //  var bounds = path.bounds(collection),
+             //      topLeft = bounds[0],
+             //      bottomRight = bounds[1];
+
+
+             //  svg.attr("width", bottomRight[0] - topLeft[0])
+             //      .attr("height", bottomRight[1] - topLeft[1])
+             //      .style("left", topLeft[0] + "px")
+             //      .style("top", topLeft[1] + "px");
+
+             //  g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+             // code here
+             //  });
+
+             console.log(d3.selectAll('.frontline' + response.element.attributes.date.nodeValue))
+
+             try {
+                 fetch("./maps/" + response.element.attributes[3].nodeValue + ".geojson")
+                     .then(function(response) {
+                         return response.json();
                      })
-                     geoData.setStyle({ 'className': 'frontline' })
-                     geoData.addTo(map);
-                 });
+                     .then(function(data) {
+                         geoData = L.geoJSON(data, {
+                             style: frontLineStyles,
+                         })
+                         geoData.setStyle({ 'className': 'frontline' + response.element.attributes.date.nodeValue })
+                         geoData.addTo(map);
+                     });
+             } catch {}
 
              circle.bindPopup(response.element.attributes[5].nodeValue, { closeButton: false }).openPopup()
 
@@ -82,7 +143,12 @@
          }
      })
      .onStepExit((response) => {
+
+         console.log("exit: " + response.index)
+         console.log('frontline' + response.element.attributes.date.nodeValue)
          map.flyTo([47.11, 37.57], 12);
+
+
 
 
          d3.selectAll('.point')
