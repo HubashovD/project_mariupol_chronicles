26c20102efe503647b72774bc1135e194c785aec
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-03-23 18:09:29 +0200

    updates

--- js/text_generator.js
+++ js/text_generator.js
@@ -13,7 +13,7 @@ d3.csv("./data/data.csv").then(function(data) {
             .attr("date", element.date)
             .attr("coords", element.coordinates)
             .attr("coords-text", element.coordinates_text)
-            .attr("header", element.header)
+            // .attr("header", element.header)
 
         d3.select("#step-" + element.step)
             .append('div')
@@ -47,11 +47,44 @@ d3.csv("./data/data.csv").then(function(data) {
                     .append('div')
             }
 
+            // d3.select("#grid-step-" + element.step)
+            //     .append('p')
+            //     .text(element.text)
+
+
             d3.select("#grid-step-" + element.step)
+                .append('div')
+                .attr("id", "inner-grid-step-" + element.step)
+                .attr("class", "inner-grid-step-mask")
+
+
+            d3.select("#inner-grid-step-" + element.step)
+                .append('p')
+                .attr("class", "inner-grid-step-header")
+                .text(element.header)
+
+            d3.select("#inner-grid-step-" + element.step)
                 .append('p')
                 .text(element.text)
+
+
         } else {
+            // d3.select("#grid-step-" + element.step)
+            //     .append('p')
+            //     .text(element.text)
+
             d3.select("#grid-step-" + element.step)
+                .append('div')
+                .attr("id", "inner-grid-step-" + element.step)
+                .attr("class", "inner-grid-step-mask")
+
+
+            d3.select("#inner-grid-step-" + element.step)
+                .append('p')
+                .attr("class", "inner-grid-step-header")
+                .text(element.header)
+
+            d3.select("#inner-grid-step-" + element.step)
                 .append('p')
                 .text(element.text)
 
