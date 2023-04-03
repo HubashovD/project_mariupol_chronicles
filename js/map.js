a0a6d620f9bcf0216f03d1c5ecc4fd2a9bde30b5
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-04-03 13:30:58 +0300

    some little updates

--- js/map.js
+++ js/map.js
@@ -7,14 +7,6 @@ var map = L.map('map').setView([47.11, 37.57], 12);
 
 
 
-
-
-
-
-
-
-
-
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
